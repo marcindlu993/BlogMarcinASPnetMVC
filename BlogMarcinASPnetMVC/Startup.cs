72dc1dce1fe8457032887e9fed11624552e1bdb8
Marcin Długoszewski <marcind993@poczta.onet.pl>
2017-07-09 09:54:59 +0200

    Add project files.

---
+++ BlogMarcinASPnetMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BlogMarcinASPnetMVC.Startup))]
+namespace BlogMarcinASPnetMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
